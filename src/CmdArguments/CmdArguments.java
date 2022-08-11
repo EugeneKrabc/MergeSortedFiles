9ef5bc27a9f3516423fab207674c69f8bb70c748
Curran Carr <curranca@un-d6.kzn.21-school.ru>
2022-08-11 18:30:55 +0300

    Finish refactoring CmdArgs class, need to test all cases

---
+++ src/CmdArguments/CmdArguments.java
@@ -0,0 +1,95 @@
+package CmdArguments;
+import java.util.ArrayList;
+
+
+public final class CmdArguments {
+    public CmdArguments(String[] args) throws Exception{
+        this.args = args;
+        parseCmdArgs();
+    }
+
+    public String getOutputFileName() {
+        return outputFile;
+    }
+
+    public ArrayList<String> getInputFileNames() {
+        return inputFiles;
+    }
+
+    private enum DataType {
+        INTEGERS,
+        STRINGS
+    }
+    private enum SortType {
+        ASCENDING,
+        DESCENDING
+    }
+
+    private String[] args;
+    private boolean haveOptionalArgument;
+    private SortType sortType = null;
+    private DataType dataType = null;
+    private String outputFile;
+    private ArrayList<String> inputFiles = new ArrayList<>();
+
+    private void parseCmdArgs() throws Exception {
+        if (args.length < 3)
+            throw new Exception("Wrong number of arguments");
+
+        getDataType(args);
+        getSortType(args);
+
+        if (dataType == null) {
+            throw new Exception("Wrong arguments: no data type specified( -i or -s)");
+        }
+
+        if (sortType == null) {
+            sortType = SortType.ASCENDING;
+            haveOptionalArgument = false;
+        } else {
+            haveOptionalArgument = true;
+        }
+
+        int outputFileIndex;
+        if (haveOptionalArgument)
+            outputFileIndex = 2;
+        else
+            outputFileIndex = 1;
+        outputFile = new String(args[outputFileIndex]);
+
+        for (int i = outputFileIndex + 1; i < args.length; i++)
+            inputFiles.add(args[i]);
+    }
+
+    private void getDataType(String[] args) throws Exception {
+        if (args[0].equals("-s") || args[1].equals("-s")) {
+            if (dataType == DataType.INTEGERS)
+                throw new Exception("Wrong date type: you should pass '-s' or '-i', not both");
+            
+            dataType = DataType.STRINGS;
+        }
+        
+        if (args[0].equals("-i") || args[1].equals("-i")) {
+            if (dataType == DataType.STRINGS)
+                throw new Exception("Wrong date type: you should pass '-s' or '-i', not both");
+            
+            dataType = DataType.INTEGERS;
+        }
+    }
+
+    private void getSortType(String[] args) throws Exception {
+        if (args[0].equals("-a") || args[1].equals("-a")) {
+            if (sortType == SortType.DESCENDING)
+                throw new Exception("Wrong sort type: you should pass '-a' or '-d', not both");
+
+            sortType = SortType.ASCENDING;
+        }
+
+        if (args[0].equals("-d") || args[1].equals("-d")) {
+            if (sortType == SortType.ASCENDING)
+                throw new Exception("Wrong sort type: you should pass '-a' or '-d', not both");
+        
+            sortType = SortType.DESCENDING;
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,94 +1,14 @@
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import CmdArguments.CmdArguments;
 
 public class Main {
-    private enum DataType {
-        INTEGERS,
-        STRINGS
-    }
-    private enum SortType {
-        ASCENDING,
-        DESCENDING
-    }
-
-    private static boolean haveOptionalArgument;
-    private static SortType sortType = null;
-    private static DataType dataType = null;
-    private static String outputFile;
-    private static ArrayList<String> inputFiles = new ArrayList<>();
-
-    static void parseCmdArgs(String[] args) throws Exception {
-        if (args.length < 3)
-            throw new Exception("Wrong number of arguments");
-
-        getDataType(args);
-        getSortType(args);
-
-        if (dataType == null) {
-            throw new Exception("Wrong arguments: no data");
-        }
-
-        if (sortType == null) {
-            sortType = SortType.ASCENDING;
-            haveOptionalArgument = false;
-        } else {
-            haveOptionalArgument = true;
-        }
-
-        int outputFileIndex;
-        if (haveOptionalArgument)
-            outputFileIndex = 2;
-        else
-            outputFileIndex = 1;
-        outputFile = new String(args[outputFileIndex]);
-
-        for (int i = outputFileIndex + 1; i < args.length; i++)
-            inputFiles.add(args[i]);
-    }
-
-    static void getDataType(String[] args) throws Exception {
-        if (args[0].equals("-s") || args[1].equals("-s")) {
-            if (dataType == DataType.INTEGERS)
-                throw new Exception("Wrong date type: you should pass '-s' or '-i', not both");
-            
-            dataType = DataType.STRINGS;
-        }
-        
-        if (args[0].equals("-i") || args[1].equals("-i")) {
-            if (dataType == DataType.STRINGS)
-                throw new Exception("Wrong date type: you should pass '-s' or '-i', not both");
-            
-            dataType = DataType.INTEGERS;
-        }
-    }
-
-    static void getSortType(String[] args) throws Exception {
-        if (args[0].equals("-a") || args[1].equals("-a")) {
-            if (sortType == SortType.DESCENDING)
-                throw new Exception("Wrong sort type: you should pass '-a' or '-d', not both");
-
-            sortType = SortType.ASCENDING;
-        }
-
-        if (args[0].equals("-d") || args[1].equals("-d")) {
-            if (sortType == SortType.ASCENDING)
-                throw new Exception("Wrong sort type: you should pass '-a' or '-d', not both");
-        
-            sortType = SortType.DESCENDING;
-        }
-    }
-    
-
     public static void main(String[] args) {
         try {
-            parseCmdArgs(args);
-            System.out.println(inputFiles);
+            CmdArguments argsData = new CmdArguments(args);
+            System.out.println("Output file name: " + argsData.getOutputFileName());
+            System.out.println("Input file names: " + argsData.getInputFileNames());
         } catch (Exception e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-
     }
 }
