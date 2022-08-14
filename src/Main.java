ff51a954fd5e54937c83717c592ed31d6ed347f4
Curran Carr <curranca@un-d6.kzn.21-school.ru>
2022-08-14 13:34:47 +0300

    Split main sorting function, need to handle case with unsorted input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
@@ -14,46 +15,55 @@ public class Main {
     public static void main(String[] args) {
         try {
             CmdArgumentsParser argsData = new CmdArgumentsParser(args);
-            System.out.println("Output file name: " + argsData.getOutputFileName());
-            System.out.println("Input file names: " + argsData.getInputFileNames());
             FilesHandler files = new FilesHandler(argsData.getOutputFileName(), argsData.getInputFileNames());
-            inputFiles = files.getInputFiles();
-            outputFile = files.getOutputFile();
-            sortType = argsData.getSortType();
-            dataType = argsData.getDataType();
-            System.out.println("SortType: " + sortType);
-            System.out.println("DataType: " + dataType);
+            preparetionForSorting(argsData, files);
             mergeSortFiles();
-
         } catch (Exception e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
-
     }
 
-    private static ArrayList<File> inputFiles;
-    private static File outputFile;
+    private static Scanner[] inputFileScanners;
+    private static String[] arrWithLastScannedValues;
+    private static int scannerIndexWithMinValue = 0;
+    private static int finalValueToWrite = 0;
+    private static String finalStringToWrite = null;
+
+    private static FileWriter outputWriter;
     
     private static SortType sortType;
     private static DataType dataType;
 
+    private static HashSet<Integer> emptyFilesIndexes = new HashSet<>();
 
-    private static void mergeSortFiles() throws Exception {
-        FileWriter outputWriter = new FileWriter(outputFile);
-        Scanner[] inputFileScanners = new Scanner[inputFiles.size()];
+    private static void preparetionForSorting(CmdArgumentsParser argsData, FilesHandler files) throws IOException {
+        ArrayList<File> inputFiles = files.getInputFiles();
+        File outputFile = files.getOutputFile();
+        sortType = argsData.getSortType();
+        dataType = argsData.getDataType();
+
+        outputWriter = new FileWriter(outputFile);
+        inputFileScanners = new Scanner[inputFiles.size()];
         for (int i = 0; i < inputFiles.size(); i++)
             inputFileScanners[i] = new Scanner(inputFiles.get(i));
         
-        int scannerIndexWithMinValue = 0;
-        HashSet<Integer> emptyFilesIndexes = new HashSet<>();
-        String[] arrWithLastScannedValues = new String[inputFileScanners.length];
-        int finalValueToWrite = 0;
-        String finalStringToWrite = null;
-        if (sortType == SortType.ASCENDING)
-            finalValueToWrite = Integer.MAX_VALUE;
-        else
-            finalValueToWrite = Integer.MIN_VALUE;
+        arrWithLastScannedValues = new String[inputFileScanners.length];
+    }
+
+    private static void mergeSortFiles() throws IOException {
+        try {
+            mainSortingCycle();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            for (Scanner scanner : inputFileScanners)
+                scanner.close();
+            outputWriter.close();
+        }
+    }
+
+    private static void mainSortingCycle() throws IOException {
+        resetFinalValueToWrite();
         while (emptyFilesIndexes.size() != inputFileScanners.length) {
             for (int i = 0; i < inputFileScanners.length; i++) {
                 if (arrWithLastScannedValues[i] == null) {
@@ -72,21 +82,16 @@ public class Main {
                         finalStringToWrite = arrWithLastScannedValues[i];
                 }
             }
+
             if (emptyFilesIndexes.size() != inputFileScanners.length) {
                 if (dataType == DataType.STRINGS && finalStringToWrite != null)
                     outputWriter.write(finalStringToWrite + "\n");
                 else
                     outputWriter.write(finalValueToWrite + "\n");
                 arrWithLastScannedValues[scannerIndexWithMinValue] = null;
-                if (sortType == SortType.ASCENDING)
-                    finalValueToWrite = Integer.MAX_VALUE;
-                else
-                    finalValueToWrite = Integer.MIN_VALUE;
+                resetFinalValueToWrite();
             }
         }
-        for (Scanner scanner : inputFileScanners)
-            scanner.close();
-        outputWriter.close();
     }
 
     private static int getValueDependsOnDataType(String line) {
@@ -101,6 +106,13 @@ public class Main {
             return currValue < finalValueToWrite;
         else
             return currValue > finalValueToWrite;
+    }
+
+    private static void resetFinalValueToWrite() {
+        if (sortType == SortType.ASCENDING)
+            finalValueToWrite = Integer.MAX_VALUE;
+        else
+            finalValueToWrite = Integer.MIN_VALUE;
     }
 
 }
