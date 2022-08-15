4ce180f853ee6adaed69507b1595497de1756c4e
Curran Carr <curranca@un-d6.kzn.21-school.ru>
2022-08-15 14:20:12 +0300

    Refactor CmdArgs

--- src/main/java/helperclasses/CmdArguments.java
+++ src/main/java/helperclasses/CmdArguments.java
@@ -1,4 +1,5 @@
 package helperclasses;
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public final class CmdArguments {
@@ -23,19 +24,27 @@ public final class CmdArguments {
         return sortType;
     }
 
+//    public enum SortType {
+//        ASCENDING,
+//        DESCENDING
+//    }
+//    public enum DataType {
+//        INTEGERS,
+//        STRINGS
+//    }
+    private SortType sortType;
+    private DataType dataType;
     private final String[] args;
-    private SortType sortType = null;
-    private DataType dataType = null;
     private String outputFile;
     private final LinkedList<String> inputFiles = new LinkedList<>();
 
     private void parseCmdArgs() throws Exception {
-        if (args.length < 3)
+        if (args.length < 3) {
             throw new Exception("Wrong number of arguments.");
+        }
 
-        getDataType(args);
-        getSortType(args);
-
+        parseDataType(args);
+        parseSortType(args);
         if (dataType == null) {
             throw new Exception("Wrong arguments: no data type specified( -i or -s)");
         }
@@ -49,46 +58,50 @@ public final class CmdArguments {
         }
 
         int outputFileIndex;
-        if (haveOptionalArgument)
+        if (haveOptionalArgument) {
             outputFileIndex = 2;
-        else
+        } else {
             outputFileIndex = 1;
+        }
         outputFile = args[outputFileIndex];
-
-        for (int i = outputFileIndex + 1; i < args.length; i++)
-            inputFiles.add(args[i]);
+        inputFiles.addAll(Arrays.asList(args).subList(outputFileIndex + 1, args.length));
     }
 
-    private void getDataType(String[] args) throws Exception {
-        if (args[0].equals(args[1]))
+    private void parseDataType(String[] args) throws Exception {
+        if (args[0].equals(args[1])) {
             throw new Exception("Wrong arguments: identical flags are not allowed");
+        }
 
         if (args[0].equals("-s") || args[1].equals("-s")) {
-            if (dataType == DataType.INTEGERS)
+            if (dataType == DataType.INTEGERS) {
                 throw new Exception("Wrong date type: you should pass '-s' or '-i', not both");
+            }
 
             dataType = DataType.STRINGS;
         }
 
         if (args[0].equals("-i") || args[1].equals("-i")) {
-            if (dataType == DataType.STRINGS)
+            if (dataType == DataType.STRINGS) {
                 throw new Exception("Wrong date type: you should pass '-s' or '-i', not both");
+            }
 
             dataType = DataType.INTEGERS;
         }
     }
 
-    private void getSortType(String[] args) throws Exception {
+    private void parseSortType(String[] args) throws Exception {
         if (args[0].equals("-a") || args[1].equals("-a")) {
-            if (sortType == SortType.DESCENDING)
+            if (sortType == SortType.DESCENDING) {
                 throw new Exception("Wrong sort type: you should pass '-a' or '-d', not both");
+            }
 
             sortType = SortType.ASCENDING;
         }
 
         if (args[0].equals("-d") || args[1].equals("-d")) {
-            if (sortType == SortType.ASCENDING)
+            if (sortType == SortType.ASCENDING) {
                 throw new Exception("Wrong sort type: you should pass '-a' or '-d', not both");
+            }
 
             sortType = SortType.DESCENDING;
         }
